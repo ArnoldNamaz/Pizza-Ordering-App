6f322c9b74667d073da44eb4c04db7f75ca7a580
Arnold Namaz <namaonamarshall1@gmail.com>
2018-05-25 22:07:02 -0700

    Initial Commit

---
+++ Pizza Ordering App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizza Ordering App
+//
+//  Created by Marshall on 25/5/2018.
+//  Copyright © 2018 Muzinda Group project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
